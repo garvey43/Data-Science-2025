cce3544ac7242d13ba6fda612e08cd74424c154d
Hacketbatch <107368632+Hacketbatch@users.noreply.github.com>
2025-10-17 11:34:22 +0300

    Update update-dashboard.yml
    used a link instead of direct path

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ class ProfessionalDashboard {
 
     async loadCSVData() {
         try {
-            const response = await fetch('./completion_analysis.csv');
+            const response = await fetch('https://raw.githubusercontent.com/Eldohub-data-scientists/Data-Science-2025/main/completion_analysis.csv');
             if (!response.ok) {
                 throw new Error('CSV file not found');
             }
